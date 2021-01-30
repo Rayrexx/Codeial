30d65ab159c47575558dc45d867d88fbcf4523c0
Harsh <harshkaushik1997@gmail.com>
2021-01-30 15:14:54 +0530

    display posts and authors name on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,32 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: 'Home'
-    });
-};
+    //console.log(req.cookies);
+    //res.cookie('user_id',25);
+    
+    //Method 1:- Showing posts without populating the user(facing issues while getting to show the username with posts)
+
+    // Post.find({}, function(err, posts){
+    //     if(err){console.log('Error in fetching posts'); return;}
+
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         posts: posts
+            
+    //     });   
+    
+    // });
+
+    //Method 2:-
+    //Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+            if(err){console.log('Error in fetching posts'); return;}
+    
+            return res.render('home', {
+                title: 'Home',
+                posts: posts
+                
+            });
+
+        })
+}
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const homeController = require('../controllers/home_controller');
 //Home page
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 
 //exporting router
